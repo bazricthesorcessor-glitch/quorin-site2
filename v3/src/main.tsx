c1c50589cc4291cbc45b34ef72d7572929fbbd4f
bazricthesorcessor-glitch <bazricthesorcessor@gmail.com>
2026-07-14 23:53:23 +0530

    feat(v3): provide shared commerce state

--- v3/src/main.tsx
+++ v3/src/main.tsx
@@ -2,5 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import { App } from "./app/App";
+import { ShopProvider } from "./state/ShopContext";
 import "./app/styles/global.css";
-createRoot(document.getElementById("root")!).render(<StrictMode><BrowserRouter><App /></BrowserRouter></StrictMode>);
+createRoot(document.getElementById("root")!).render(<StrictMode><BrowserRouter><ShopProvider><App /></ShopProvider></BrowserRouter></StrictMode>);
